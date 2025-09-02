b030a3b6b2f8b1d3472b293f300eb58f12206223
Yugz29 <yannduzelier@gmail.com>
2025-09-02 07:39:58 +0200

    fix: conditional statements

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ print(f"Last digit of {number} is {lastnumber} ", end ="")
 if lastnumber > 5:
     print("and is greater than 5")
 
-if lastnumber == 0:
+elif lastnumber == 0:
     print("and is 0")
 
-if lastnumber < 6 and not 0:
+elif lastnumber < 6 and not 0:
     print("and is less than 6 and not 0")
