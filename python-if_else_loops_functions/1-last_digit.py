1d9f9f9e7bd4df58331eae773c2a791f7d6511c7
Yugz29 <yannduzelier@gmail.com>
2025-09-02 07:38:43 +0200

    add: new variable, conditional statements

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastnumber = int(str(number)[-1])
+
+print(f"Last digit of {number} is {lastnumber} ", end ="")
+
+if lastnumber > 5:
+    print("and is greater than 5")
+
+if lastnumber == 0:
+    print("and is 0")
+
+if lastnumber < 6 and not 0:
+    print("and is less than 6 and not 0")
