3dd7adac6b29d248f76c52e370ebdf34b5d04787
Yugz29 <yannduzelier@gmail.com>
2025-09-05 10:16:41 +0200

    add: conditions and loop

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
