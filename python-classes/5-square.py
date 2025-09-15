b5eeda78b38155f59ce8cc56a514bc2b65bcb96f
Yugz29 <yannduzelier@gmail.com>
2025-09-15 13:21:53 +0200

    add: def my_print

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module square
+Defines an empty class Square.
+"""
+
+
+class Square:
+    """Represents an empty Square class.
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, valeur):
+        if type(valeur) is not int:
+            raise TypeError("size must be an integer")
+        if valeur < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = valeur
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
