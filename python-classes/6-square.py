2547a96283dc8ba58140b9ff90271eebd4352e36
Yugz29 <yannduzelier@gmail.com>
2025-09-15 14:36:12 +0200

    remove: return fix: '' to ' '

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,9 +45,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
-            return
         for _ in range(self.__position[1]):
             print("")
             return
         for _ in range(self.__size):
-            print("" * self.__position[0] + "#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
