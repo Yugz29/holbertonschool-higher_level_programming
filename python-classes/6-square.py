549c211da473c16e4559906bfb00ff3c10a7c05d
Yugz29 <yannduzelier@gmail.com>
2025-09-15 14:41:35 +0200

    add: return

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,6 +45,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
+            return
         for _ in range(self.__position[1]):
             print("")
         for _ in range(self.__size):
