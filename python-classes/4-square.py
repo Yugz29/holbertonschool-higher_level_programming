451292bb95ccbbac032be7aa9a4432462e2bd155
Yugz29 <yannduzelier@gmail.com>
2025-09-15 13:14:57 +0200

    add: getter/setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module square
+Defines an empty class Square.
+"""
+
+
+class Square:
+    """Represents an empty Square class.
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, valeur):
+        if type(valeur) is not int:
+            raise TypeError("size must be an integer")
+        if valeur < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = valeur
