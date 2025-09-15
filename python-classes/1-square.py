beca3dea9728f7d165f8146c79c6ba7b2196dd5b
Yugz29 <yannduzelier@gmail.com>
2025-09-15 12:54:28 +0200

    add: attribut

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module square
+Defines an empty class Square.
+"""
+
+
+class Square:
+    """Represents an empty Square class.
+    """
+    def __init__(self, size):
+        self.__size = size
