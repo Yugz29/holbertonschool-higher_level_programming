00959a41aedcd13596093320ecbdd184f3f08fbd
Yugz29 <yannduzelier@gmail.com>
2025-09-22 10:11:06 +0200

    add: documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Module lookup
+
+This module provides a helper function to list all
+attributes and methods available for a given object
+using Python's built-in dir() function.
+"""
+
+
 def lookup(obj):
     print(dir(obj))
