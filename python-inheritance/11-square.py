f84f76436f0aa2761d9bd78e5bcb689fab28cff8
Yugz29 <yannduzelier@gmail.com>
2025-09-23 22:17:32 +0200

    add: square return

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Defines a Rectangle subclass Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Define square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Return the string representation of the square
+        """
+        return f"[Square] {self.__size}/{self.__size}"
