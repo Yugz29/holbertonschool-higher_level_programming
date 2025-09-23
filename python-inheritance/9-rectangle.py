d7f68be9e62afe37fa37b22906cac41f09129d31
Yugz29 <yannduzelier@gmail.com>
2025-09-23 19:27:50 +0200

    add: area and str method

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+
+Defines the Rectangle class that inherits from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Represents a rectangle shape.
+
+    Inherits from BaseGeometry. Width and height must be
+    positive integers validated using integer_validator.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle instance.
+
+        Raises:
+            TypeError: If width or height are not integers
+            ValueError: If width or height <= 0
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Return the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return the string representation of the rectangle
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
