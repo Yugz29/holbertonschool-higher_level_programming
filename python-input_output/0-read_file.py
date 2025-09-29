2818579ac900a2ce282ac9372df67e82a319af1d
Yugz29 <yannduzelier@gmail.com>
2025-09-29 11:19:02 +0200

    add: documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Module for reading and displaying the contents of a text file"""
+
 
 def read_file(filename=""):
-    with open("my_file_0.txt", "r") as f:
+    """Reads a text file and displays its contents on standard output
+
+    Returns:
+        This function does not return anything, it only displays the content.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
         contenu = f.read()
-        print(contenu)
+        print(contenu, end="")
