1592dc7a456a2833fb450472d212983b44626a7d
Yugz29 <yannduzelier@gmail.com>
2025-10-02 09:46:12 +0200

    add: documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""
+This script adds all command line arguments to a Python list stored
+in a JSON file.
+"""
 from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+"""
+Load the current list from 'add_item.json' if it exists, otherwise start with
+an empty list.
+Append all command line arguments (excluding the script name) to this list.
+Save the updated list back to 'add_item.json'.
+"""
 try:
     arglist = load_from_json_file("add_item.json")
 except FileNotFoundError:
