97152655a4eb2fb5667c1faebda6ff9855b64799
Yugz29 <yannduzelier@gmail.com>
2025-10-02 09:33:32 +0200

    add: try/except

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    arglist = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arglist = []
+
+arglist.extend(argv[1:])
+save_to_json_file(arglist, "add_item.json")
