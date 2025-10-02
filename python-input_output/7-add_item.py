6a9cda0d07225a644a0ae4b27f9b84cfa952d6c5
Yugz29 <yannduzelier@gmail.com>
2025-10-02 09:55:39 +0200

    try to fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,15 +4,23 @@ This script adds all command line arguments to a Python list stored
 in a JSON file.
 """
 from sys import argv
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    arglist = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    arglist = []
 
-arglist.extend(argv[1:])
-save_to_json_file(arglist, "add_item.json")
+def main():
+    """
+    Load a list from 'add_item.json', add command line arguments to it,
+    and save the updated list back to the file.
+    """
+    try:
+        arglist = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arglist = []
+
+    arglist.extend(argv[1:])
+    save_to_json_file(arglist, "add_item.json")
+
+
+if __name__ == "__main__":
+    main()
