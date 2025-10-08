b526fe7555eaf9f217e23f5d5c08fca545d2d05e
Yugz29 <yannduzelier@gmail.com>
2025-10-08 21:47:51 +0200

    fix: pycodestyle

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,18 +6,22 @@ app = Flask(__name__)
 
 users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route("/data")
 def data():
     usernames = list(users.keys())
     return jsonify(usernames)
 
+
 @app.route("/status")
 def status():
     return "OK"
+
 
 @app.route("/users/<username>")
 def get_user(username):
@@ -26,8 +30,9 @@ def get_user(username):
         return jsonify(user)
     else:
         return jsonify({"error": "User not found"}), 404
-    
-@app.route("/add_user", methods = ['POST'])
+
+
+@app.route("/add_user", methods=['POST'])
 def add_user():
     data = request.get_json()
     username = data.get("username")
@@ -42,4 +47,6 @@ def add_user():
 
     return jsonify(users[username]), 201
 
-if __name__ == "__main__": app.run()
+
+if __name__ == "__main__":
+    app.run()
