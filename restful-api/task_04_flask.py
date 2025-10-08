678ab823bc03dbcd2337f5610acd9f169daff433
Yugz29 <yannduzelier@gmail.com>
2025-10-08 21:43:10 +0200

    add: routes

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route("/data")
+def data():
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+    
+@app.route("/add_user", methods = ['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[username] = {
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+
+    return jsonify(users[username]), 201
+
+if __name__ == "__main__": app.run()
