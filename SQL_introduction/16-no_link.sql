2bf9e2688902153d6ca83f03265a6cfa402da589
Yugz29 <yannduzelier@gmail.com>
2025-10-21 16:07:37 +0200

    add: more comment

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- list all records of the table second_table
+-- list all records of the table second_table not null or empty order by desc
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
