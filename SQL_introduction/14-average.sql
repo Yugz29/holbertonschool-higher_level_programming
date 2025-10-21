f31110b4652e5bfb3aa9dc05fedb27eb25f4e4e2
Yugz29 <yannduzelier@gmail.com>
2025-10-21 15:15:15 +0200

    add: CHAR(10) for line break

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- compute the score average of all records in the table second_table
-SELECT CONCAT('average of ', AVG(score), ' records') FROM second_table;
+SELECT CONCAT('average', CHAR(10), AVG(score)) FROM second_table;
