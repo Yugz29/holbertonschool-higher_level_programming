b6085e95e8f19fda0f249fe1aac35f29a8567ca9
Yugz29 <yannduzelier@gmail.com>
2025-11-08 16:26:27 +0100

    add: factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const n = parseInt(args[0]);
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(n));
