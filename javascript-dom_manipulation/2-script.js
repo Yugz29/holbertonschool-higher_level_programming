30d7a8e46c8103307746f6d53bae0ab8da52e6ac
Yugz29 <yannduzelier@gmail.com>
2025-11-10 14:40:41 +0100

    add: 'red' class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+const redHeader = document.querySelector('#red_header');
+const header = document.querySelector('header');
+
+
+redHeader.addEventListener('click', () => {
+  redHeader.classList.add('red');
+});
