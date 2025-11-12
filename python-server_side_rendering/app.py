0084ae7d567b297b4e3292861c1d60410a8947ad
Yugz29 <yannduzelier@gmail.com>
2025-11-12 15:47:41 +0100

    fix: with open ()

--- python-server_side_rendering/app.py
+++ python-server_side_rendering/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/items')
 def items():
-    with open ('items.json') as f:
+    with open('items.json') as f:
         data = json.load(f)
     items_list = data.get('items', [])
     return render_template('items.html', items=items_list)
